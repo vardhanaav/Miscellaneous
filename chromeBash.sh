880133eaaf847cf1973b07d1d94ae1814cd76d40
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2018-02-01 02:19:17 +0530

    Update chromeBash.sh

--- chromeBash.sh
+++ chromeBash.sh
@@ -1,5 +1,6 @@
 #this script installs google-chrome on kali
 #https://linuxconfig.org/how-to-install-google-chrome-browser-on-kali-linux
+# I'm assuming that this installation comes from a user other than root. Will update this soon for root users as well. 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install gdebi
 gdebi google-chrome-stable_current_amd64.deb
